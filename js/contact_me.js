df076161765ea91ee54b91ea5bd175aba0704c50
Math-QA <math.qa.dataset@gmail.com>
2019-03-30 00:07:18 -0700

    email fix

--- js/contact_me.js
+++ js/contact_me.js
@@ -20,7 +20,6 @@ $(function() {
       $this = $("#sendMessageButton");
       $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
       $.ajax({
-        alert("shdkjskjahks")
         url: "././mail/contact_me.php",
 
         type: "POST",
